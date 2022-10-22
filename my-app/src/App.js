acdece1b774881b1c4ee7cb645ebbcb12600e488
favorcat <favorcat.me@gmail.com>
2022-10-22 22:28:17 +0900

    add react-router-dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import MainPage from './pages/main/mainPage';
 
-import { db } from './firebase';
-import { collection, getDocs } from "firebase/firestore";
-
-async function App() {
-
-  const querySnapshot = await getDocs(collection(db, "MenuData"));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-      console.log(`${doc.data().name} , ${doc.data().price}원, ${doc.data().type}, ${doc.data().cafeteria.name}`);
-    });
-
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='header'>
+        <div className='title'>꼬르륵</div>
+        <div className='userbtn'>
+          리뷰 마이페이지
+        </div>
+        <div className='search-container'></div>
+      </div>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
