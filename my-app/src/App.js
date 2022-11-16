a57817644f7f7c7636b7d767e01378cc05575a0e
favorcat <favorcat.me@gmail.com>
2022-11-16 22:37:49 +0900

    feat: 로그인, 로그아웃 구현

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,42 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useCookies } from 'react-cookie';
 
 import React from 'react';
+import axios from 'axios';
 
 import LoginPage from './pages/login/login';
-import SignupPage from './pages/signup/signup';
+import SignupPage from './pages/register/register';
 import MainPage from './pages/main/mainPage';
 
 import TypePage from './pages/menu/typeDetail';
 import CafePage from './pages/cafeteria/cafeteriaDetail';
 
 function App() {
+  // const location = useLocation()
+  const location = window.location.pathname;
+  const [cookie, setCookie, removeCookie] = useCookies(['token']);
+
+  // React.useEffect(() => { // useEffect가 location이 바뀔 때마다 실행됨
+  //   if (cookie.token) { // 만약 토큰이 존재하면
+  //     // 토큰 검증을 함
+  //     axios.get('http://localhostㄹ:8000/api/verifytoken', { headers: { Authorization: `Bearer ${cookie.token}` } })
+  //       .then((res) => {
+  //         // 요청 성공 시 그냥 지나감
+  //         console.log(res.data);
+  //       })
+  //       .catch((err) => {
+  //         // 에러 발생 시 쿠키 삭제
+  //         removeCookie('token');
+  //       })
+  //   }
+  // }, [location])
+
+  function logout(){
+    removeCookie('token');
+    window.location.href = '/';
+  }
+
   const onClick=() => {
     alert("헬로");
   }
@@ -26,7 +51,12 @@ function App() {
         <div className='title-container'>
           <div className='title'><a href='/'>꼬르륵</a></div>
           <div className='userbtn'>
-            <a href="/login">my page</a>
+            {cookie.token != null
+            ? <div>
+              {/* <a href='/mypage'>마이페이지</a> */}
+              <button className="logout" onClick={logout}> 로그아웃</button>
+              </div>
+            : <a className="login" href='/login'>로그인</a>}
           </div>
         </div>
         <div className='search-container'>
